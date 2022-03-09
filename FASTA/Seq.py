cf9f4c7eb1ecaea38ed063ccc39046dc6305c577
Je-Young Woo <jeyoung@Je-Youngui-MacBookPro.local>
2022-03-09 16:35:42 +0900

    Add __add__ special method and concatenate method of Seq() type

--- FASTA/Seq.py
+++ FASTA/Seq.py
@@ -33,6 +33,18 @@ class Seq:
 
     def __str__(self):
         return str(self.data)
+    
+    def __add__(self, other_seq):
+        if type(Seq('A')) == type(other_seq):
+            add_seq = str(other_seq)
+        elif type('A') == type(other_seq):
+            add_seq = other_seq
+        else:
+            raise TypeError('The type of "other_seq" must be str or Seq() type.')
+        return Seq(self.data + add_seq)
+
+    def concatenate(self, other_seq):
+        return self.__add__(other_seq)
 
     def complement(self):
         return Seq("".join([REVC[base] for base in self.data]))
@@ -49,5 +61,21 @@ if __name__ == "__main__":
     test_seq = 'ATGCTAGTCAGTCGTAGCTATTTGTACGTATCGATCTACTAGC'
     print(test_seq)
     
-    temp = Seq(test_seq)
-    print(temp.reverse_complemnt())
+    test1_seq = Seq('AAAAAAAA')
+    test2_seq = Seq('GGGGGGGG')
+
+    print(test1_seq, test1_seq.complement())
+    print(test2_seq, test2_seq.complement())
+    print((test1_seq + test2_seq))
+    print((test1_seq + test2_seq).reverse())
+    print((test1_seq + test2_seq).complement())
+    print((test1_seq + test2_seq).reverse_complemnt())
+    
+    print(test1_seq.concatenate(test2_seq))
+    print(test1_seq.concatenate("ATGACGTACGTAGCT"))
+    
+    print(test1_seq + 'AGCTAGCTGATCGTGGGCTAGTCGTAGCTG')
+    print(test1_seq + 1)
+    
+    #temp = Seq(test_seq)
+    #print(temp.reverse_complemnt())
