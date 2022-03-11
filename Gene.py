1cc24eedad914886c7244fa30c2433734e10b780
Beomman-Han <qjaaks6378@gmail.com>
2022-03-12 02:12:20 +0900

    Create .gitignore file

---
+++ Gene.py
@@ -0,0 +1,33 @@
+class Gene(object):
+    """class reflects real gene's attributes
+    and supports various functions for dealing with jobs about gene
+
+    Most of gene's character are saved in UCSC refgene database
+    +
+    
+    Gene's characteristics
+    - It locates across specific region in genome (= it has DNA sequence)
+    - It has 'exons' and 'introns' -> all possible transcripts???
+    - It goes through 'splicing' -> motif for splicing
+    - It has 'promotor' at the upstream location
+    - It could be transcripted
+    - It could be multiple transcript
+    
+    - length 
+        DNA - ~~~
+        
+        Transcript1 (RNA) - ~~~
+        Protein (amino acid) - ~~~
+        
+        Transcript2 (RNA) - ~~~
+        ..
+        
+    
+    - Related disease....
+    - 
+    
+    """
+    
+class transcript():
+    """"""
+    pass
