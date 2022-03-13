beeb6559b871b6e7d5f6150e700321d3b1a1f5e9
Beomman-Han <qjaaks6378@gmail.com>
2022-03-13 14:35:09 +0900

    Add interfaces of FileProcessor class

--- FASTA/BAM.py
+++ FASTA/BAM.py
@@ -10,8 +10,31 @@ class BAMProcessor(FileProcessor):
     ----------
     FileProcessor : FileProcessor
         Parent class
+    
+    Interfaces
+    ----------
+    sanity_check()
+    open()
+    close()
+    readline()
+    write()
+    import_from_json()
+    export_to_json()
     """
     
     def __init__(self):
+        pass    
+    def sanity_check(self):
         pass
-    
+    def open(self):
+        pass
+    def close(self):
+        pass
+    def readline(self):
+        pass
+    def write(self):
+        pass
+    def import_from_json(self):
+        pass
+    def export_to_json(self,):
+        pass
