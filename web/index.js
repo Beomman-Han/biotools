7bf3234b37bf848ffa10755257f08b57787fc87a
Beomman-Han <qjaaks6378@gmail.com>
2022-03-17 00:11:06 +0900

    Add event handler for click event at 'CLEAR' button

--- web/index.js
+++ web/index.js
@@ -1,4 +1,4 @@
-// Get submit button object
+// Get 'SUBMIT' button object
 const submit_btn = document.querySelector(".content #submit");
 
 // Event handler for click at SUBMIT button
@@ -6,6 +6,16 @@ function submit() {
     const input = document.querySelector(".content textarea");
     console.log(input.value);
 }
-
 // Add event handler
 submit_btn.addEventListener('click', submit);
+
+// Get 'CLEAR' button object
+const clear_btn = document.querySelector(".content #clear");
+
+// Event handler
+function clear() {
+    const input = document.querySelector(".content textarea");
+    input.value = "";
+}
+// Add event handler
+clear_btn.addEventListener('click', clear);
